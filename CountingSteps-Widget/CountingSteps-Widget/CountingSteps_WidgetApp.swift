e65f3641a7a35e11f00fcc12cf00106fd8180c1f
Chgunz <andyboy420019@gmail.com>
2025-04-15 14:15:49 -0700

    Added xcode project to github repository

---
+++ CountingSteps-Widget/CountingSteps-Widget/CountingSteps_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountingSteps_WidgetApp.swift
+//  CountingSteps-Widget
+//
+//  Created by Kevin Lopez on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CountingSteps_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
